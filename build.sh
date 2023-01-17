9798f66d74da27c2c69d826c8dccac5a9521d1c5
yan <floraryz@gmail.com>
2023-01-17 10:11:03 -0800

    adding files

---
+++ build.sh
@@ -0,0 +1,4 @@
+cat templates/top.html content/index.html templates/bottom.html > ssg-bash/index.html
+cat templates/top.html content/blog.html templates/bottom.html > ssg-bash/blog.html
+cat templates/top.html content/projects.html templates/bottom.html > ssg-bash/projects.html
+cat templates/top.html content/contact.html templates/bottom.html > ssg-bash/contact.html
